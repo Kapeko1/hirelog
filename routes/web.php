e0b2c1dbad81365258336ff2092a7439a6d331f8
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2025-05-02 00:33:33 +0200

    add new route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/documents/{document}/download', [DocumentController::class, 'download'])
     ->middleware(['auth'])
     ->name('documents.download');
+
+Route::get('/', function () {
+    return redirect('/admin');
+});
