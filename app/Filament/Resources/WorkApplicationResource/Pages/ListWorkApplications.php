315ff206223c75f4d2d043f0400b004ee98c3bbb
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2025-07-04 13:05:01 +0200

    feat: create new filtering on Work Applications based on status

--- app/Filament/Resources/WorkApplicationResource/Pages/ListWorkApplications.php
+++ app/Filament/Resources/WorkApplicationResource/Pages/ListWorkApplications.php
@@ -5,6 +5,10 @@ namespace App\Filament\Resources\WorkApplicationResource\Pages;
 use App\Filament\Resources\WorkApplicationResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use App\Enums\ApplicationStatus;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ListWorkApplications extends ListRecords
 {
@@ -14,6 +18,34 @@ class ListWorkApplications extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('Wszystkie'),
+            'applied' => Tab::make('Złożono')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status',
+                    ApplicationStatus::Applied)),
+            'verification' => Tab::make('Weryfikacja')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status',
+                    ApplicationStatus::Verification)),
+            'interview' => Tab::make('Rozmowa')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status',
+                    ApplicationStatus::Interview)),
+            'offer' => Tab::make('Oferta')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status',
+                    ApplicationStatus::Offer)),
+            'positive' => Tab::make('Pozytywne')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status',
+                    ApplicationStatus::Hired)),
+            'negative' => Tab::make('Negatywne')
+                ->modifyQueryUsing(fn (Builder $query) =>
+                $query->whereIn('status', [
+                    ApplicationStatus::Rejected,
+                    ApplicationStatus::Ghosted
+                ])),
         ];
     }
 }
