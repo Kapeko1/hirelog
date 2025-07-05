3752c6955ce14aca4044b4d1a9879acebfc42f54
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2025-07-05 23:53:13 +0200

    feat: add invitation_codes table

---
+++ database/migrations/2025_07_05_213905_create_invitation_codes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitation_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')
+                ->unique();
+            $table->boolean('is_used')
+                ->default(false);
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()->onDelete('cascade');
+            $table->timestamp('used_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitation_codes');
+    }
+};
