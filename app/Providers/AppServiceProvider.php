e8db9955c8d7af8c4680546ea13bfd99b40187ca
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2026-02-02 22:45:15 +0100

    feat: register observer in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\WorkApplication;
+use App\Observers\WorkApplicationObserver;
 use BezhanSalleh\LanguageSwitch\LanguageSwitch;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Register observers
+        WorkApplication::observe(WorkApplicationObserver::class);
+
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
                 ->locales(['pl', 'en'])
