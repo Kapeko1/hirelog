f83eb72b0f0602c200b6cce17533dac9ebb8485b
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2026-02-10 23:49:25 +0100

    add is Admin user

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -13,7 +13,7 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'Kacper',
             'email' => 'kacper.gadek2@wp.pl',
             'is_admin' => true,
