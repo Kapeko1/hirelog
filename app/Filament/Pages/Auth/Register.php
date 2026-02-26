a69d4fecd027c6e00b8f703c7a0e257b5c4a76f9
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2026-02-26 20:52:34 +0100

    remove registration code

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -2,12 +2,7 @@
 
 namespace App\Filament\Pages\Auth;
 
-use App\Models\InvitationCode;
-use App\Rules\ValidInvitationCode;
-use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Validation\ValidationException;
 
 class Register extends \Filament\Auth\Pages\Register
 {
@@ -15,39 +10,10 @@ class Register extends \Filament\Auth\Pages\Register
     {
         return $schema
             ->components([
-                TextInput::make('invitation_code')
-                    ->label(__('app.invitation_code'))
-                    ->required()
-                    ->rules([
-                        'required',
-                        new ValidInvitationCode,
-                    ]),
                 $this->getNameFormComponent(),
                 $this->getEmailFormComponent(),
                 $this->getPasswordFormComponent(),
                 $this->getPasswordConfirmationFormComponent(),
             ]);
-    }
-
-    /**
-     * @throws ValidationException
-     */
-    protected function handleRegistration(array $data): Model
-    {
-        $invitationCodeValue = $data['invitation_code'];
-        unset($data['invitation_code']);
-
-        $invitationCode = InvitationCode::findByCode($invitationCodeValue);
-
-        if (! $invitationCode || ! $invitationCode->isAvailable()) {
-            throw ValidationException::withMessages([
-                'invitation_code' => 'Wrong or used code',
-            ]);
-        }
-
-        $user = parent::handleRegistration($data);
-        $invitationCode->markAsUsed($user->getKey());
-
-        return $user;
     }
 }
