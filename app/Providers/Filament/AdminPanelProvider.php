40c1341730abcfcfee390dc7c49f50317bc1fb71
Kapeko1 <117565776+Kapeko1@users.noreply.github.com>
2026-03-22 04:08:25 +0100

    fix redirect issue: same url for index and dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('app')
             ->login()
             ->registration(Register::class)
             ->colors([
